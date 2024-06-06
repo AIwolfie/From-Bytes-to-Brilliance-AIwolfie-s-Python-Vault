bb1428a5c38765ba144848a2bd8668f258930e2c
Mayank Malaviya <mayankmalaviya3@gmail.com>
2024-06-06 22:54:07 +0530

    updated

---
+++ Basics_of_Python/Beginner/01_hello_world.py
@@ -0,0 +1,4 @@
+print("Hello, World!")
+
+# This is a simple program that prints "Hello, World!" to the screen.
+# The "Hello World" program is the most basic and introductory program in any programming language. It simply prints the phrase "Hello, World!" to the screen. This program is used to illustrate the basic syntax of a language and verify that the programming environment is set up correctly.
---
+++ Basics_of_Python/Beginner/02_add_two_numbers.py
@@ -0,0 +1,27 @@
+# =>methood 1
+num1 = 10
+num2 = 20
+print("the sum of the num1 and num2 is", num1+num2)
+
+# =>method 2
+num1 = 10
+num2 = 20
+sum = num1+num2
+print("the sum of the num1 and num2 is", sum)
+
+
+
+# =>method 3
+# Program to add two numbers provided by the user
+
+# Take input from the user and converting them into integers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Add the two numbers
+sum = num1+num2
+
+# display the sum using f string
+print(f"the sum of {num1} and {num2} is {sum}.")
+
+# This program takes two numbers as input from the user and outputs their sum. It's a basic introduction to user input and simple arithmetic operations in Python.
